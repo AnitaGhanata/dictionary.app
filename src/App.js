b4f4e554e10ac3af94a055fd3b3592b54a7a2389
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-12-03 23:24:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//App.js
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Display from "./Components/Display";
+import History from "./Components/History";
 
-function App() {
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import HistoryPage from "./pages/HistoryPage";
+import WordDetailPage from "./pages/WordDetailPage";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/history" element={<HistoryPage/>} />
+          <Route path="/:word" element={<WordDetailPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/redux/actions/historyAction.js
@@ -0,0 +1,10 @@
+// historyAction.js
+import { ADD_TO_HISTORY } from "./actionTypes";
+
+export const addToHistory=(word)=>{
+    return{
+        type:ADD_TO_HISTORY,
+        payload:word
+    }
+}
+
